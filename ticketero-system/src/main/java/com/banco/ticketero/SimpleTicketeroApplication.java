65eab8708ccca469d96fd3122b9fb4aaf7750b1f
capacitacion14 <capacitacion.14@api-ux.com>
2025-12-18 00:56:16 -0300

    refactor: clean up deprecated components and update main application
    - Remove SimpleTicketBotInMemory.java (replaced by TelegramBotService)
    - Remove TelegramConfig.java (replaced by AppConfig)
    - Remove TicketLifecycleManager.java (replaced by TicketProcessingScheduler)
    - Remove outdated test files (AdminControllerTest, TicketLifecycleManagerTest, TestDataBuilder)
    - Update SimpleTicketeroApplication.java to use new service architecture
    - Streamline application startup with proper component scanning

--- ticketero-system/src/main/java/com/banco/ticketero/SimpleTicketeroApplication.java
+++ ticketero-system/src/main/java/com/banco/ticketero/SimpleTicketeroApplication.java
@@ -2,10 +2,10 @@ package com.banco.ticketero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+@EnableScheduling
 public class SimpleTicketeroApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleTicketeroApplication.class, args);
